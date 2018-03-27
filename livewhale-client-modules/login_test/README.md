0481341e43902981d317b8e8edbd3e7b5d1f308d
Alexander Romanovich <alex@whitewhale.net>
2018-03-27 14:50:57 -0400

    Add login test

---
+++ livewhale-client-modules/login_test/README.md
@@ -0,0 +1,8 @@
+This module allows an admin to preview alternate login mode settings (such as trying an SSO implementation intended to replace passwords) without impacting active logins on the instance for other users.
+
+Instructions:
+
+- Configure the "custom" settings below to provide what config settings should be overrided when toggling on the overrides for your user.
+- Use /livewhale/?login&login_toggle=1 to toggle back and forth between override settings.
+
+Requirement: LiveWhale CMS/Calendar 1.6.2+.
---
+++ livewhale-client-modules/login_test/private.application.login_test.php
@@ -0,0 +1,50 @@
+<?php
+
+$_LW->REGISTERED_APPS['login_test']=array(
+	'title'=>'Login Test',
+	'handlers'=>array('onLoad'),
+	'custom'=>array( // customize the login settings to override the defaults here
+		'LOGIN_MODE'=>'CAS', // for example...
+		'CAS_HOST'=>'xxx.host.edu',
+		'CAS_PORT'=>8443,
+		'CAS_CONTEXT'=>'/cas',
+		'CAS_CERTIFICATE_PATH'=>''
+	)
+); // configure this module
+
+class LiveWhaleApplicationLoginTest {
+
+public function onLoad() { // on application load
+global $_LW;
+if ($_LW->page=='login') { // if on the login page
+	if (!empty($_LW->_GET['login_toggle'])) { // if toggling the login paramters
+		if (empty($_COOKIE['lw_'.$_LW->uuid.'_use_login_overrides'])) {
+			setcookie('lw_'.$_LW->uuid.'_use_login_overrides', 1, $_SERVER['REQUEST_TIME']+3600, '/', $_SERVER['HTTP_HOST'], false, false);
+			echo '<h2>Login Toggle</h2>Now using overrides.';
+		}
+		else {
+			setcookie('lw_'.$_LW->uuid.'_use_login_overrides', false, $_SERVER['REQUEST_TIME']+3600, '/', $_SERVER['HTTP_HOST'], false, false);
+			echo '<h2>Login Toggle</h2>No longer using overrides.';
+		};
+		exit;
+	};
+	if (!empty($_LW->_GET['login_status'])) { // if displaying login status
+		echo '<h2>Login Status</h2><pre>';
+		echo 'Using overrides?: '.(!empty($_COOKIE['lw_'.$_LW->uuid.'_use_login_overrides']) ? 'Yes' : 'No')."\n\n";
+		print_r(array_keys($_SERVER));
+		echo '</pre>';
+		exit;
+	};
+	if (!empty($_COOKIE['lw_'.$_LW->uuid.'_use_login_overrides'])) { // if the overrides cookie is set
+		if (!empty($_LW->REGISTERED_APPS['login_test']['custom']) && is_array($_LW->REGISTERED_APPS['login_test']['custom'])) { // override the settings
+			foreach($_LW->REGISTERED_APPS['login_test']['custom'] as $key=>$val) {
+				$_LW->CONFIG[$key]=$val;
+			};
+		};
+	};
+};
+}
+
+}
+
+?>
