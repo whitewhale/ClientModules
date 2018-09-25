f4ee50af3bcf8927b4774fd4aac3897e2d80a4c4
Alexander Romanovich <alex@whitewhale.net>
2018-09-25 15:20:30 -0400

    Add cleanup mechanism

--- livewhale-client-modules/ems/global.application.ems.php
+++ livewhale-client-modules/ems/global.application.ems.php
@@ -2,7 +2,7 @@
 
 $_LW->REGISTERED_APPS['ems']=array(
 	'title'=>'EMS',
-	'handlers'=>array('onSaveSuccess', 'onAfterEdit', 'onOutput', 'onGetFeedDataFilter', 'onChangeDatabaseHost'),
+	'handlers'=>array('onSaveSuccess', 'onAfterEdit', 'onOutput', 'onGetFeedDataFilter', 'onChangeDatabaseHost', 'onBeforeLogin'),
 	'flags'=>array('no_autoload'),
 	'commands'=>array('ems-debug'=>'debug'),
 	'custom'=>array( // these settings should all be set in global.config.php, not here
@@ -296,6 +296,35 @@ return @$this->client->groups;
 public function onChangeDatabaseHost($before_host, $after_host) { // switches hostname for EMS calendars
 global $_LW;
 $_LW->dbo->sql('UPDATE livewhale_events_subscriptions SET url=REPLACE(url, '.$_LW->escape('://'.$before_host.'/').', '.$_LW->escape('://'.$after_host.'/').') WHERE url LIKE "%/ems/events/%";');
+}
+
+public function onBeforeLogin() { // on before login
+global $_LW;
+$ts=strtotime('-1 month'); // set TS for 1 month ago
+if (is_dir($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations')) {
+	if (!file_exists($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations/last_check') || filemtime($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations/last_check')<$ts) { // once a month
+		touch($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations/last_check'); // mark as cleaned
+		if ($res_dirs=@scandir($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations')) { // clear cached reservations not updated within the last 1 month
+			foreach($res_dirs as $dir) {
+				if ($dir[0]!='.') {
+					if (is_dir($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations/'.$dir)) {
+						if ($files=@scandir($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations/'.$dir)) {
+							foreach($files as $file) {
+								if ($file[0]!='.') {
+									if ($file_ts=@filemtime($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations/'.$dir.'/'.$file)) {
+										if ($file_ts<$ts) {
+											@unlink($_LW->INCLUDES_DIR_PATH.'/data/ems/reservations/'.$dir.'/'.$file);
+										};
+									};
+								};
+							};
+						};
+					};
+				};
+			};
+		};
+	};
+};
 }
 
 }
