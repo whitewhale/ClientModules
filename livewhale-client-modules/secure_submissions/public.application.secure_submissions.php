79ffce992c7b64b6a5753e2debac564c4230b193
Karl Hinze <karl@whitewhale.net>
2023-05-30 11:05:36 -0400

    Update public.application.secure_submissions.php
    Pass full url and query string through login, in case you're using GET variables.

--- livewhale-client-modules/secure_submissions/public.application.secure_submissions.php
+++ livewhale-client-modules/secure_submissions/public.application.secure_submissions.php
@@ -11,7 +11,7 @@ public function onLoad() { // on frontend request
 global $_LW;
 if ($_LW->page=='/submit/index.php') { // if on public submissions page
 	if (!$_LW->isLiveWhaleUser() && !$_LW->isSSOAuthOnlyUser()) { // require authentication
-		die(header('Location: /livewhale/?login&url=/submit/'));
+		die(header('Location: /livewhale/?login&url=' . urlencode($_SERVER['REQUEST_URI'])));
 	};
 };
 }
