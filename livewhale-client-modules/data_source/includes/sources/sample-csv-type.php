a9b6f9542ff5ba1d87cbce2daa32e6feaca4064f
Karl Hinze <karl@whitewhale.net>
2023-06-08 13:35:28 -0400

    Add sample CSV config

---
+++ livewhale-client-modules/data_source/includes/sources/sample-csv-type.php
@@ -0,0 +1,11 @@
+<?php
+
+$_LW->data_source=[
+	'type'=>'csv',
+	'path'=>$_LW->INCLUDES_DIR_PATH.'/integrations/courses/courses.csv',
+	'separator'=>',',
+	'enclosure'=>'"',
+	'escape'=>'\\'
+];
+
+?>
